5702510f39be9a04cecc50a8a29c5a23df6b1e92
Jvmpers <tomaspastore200111@gmail.com>
2022-11-14 20:23:31 -0300

    nav hecho casi

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -4,14 +4,25 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import LandingPage from './Components/LandingPage/LandingPage';
+import Nav from './Components/Nav/Nav';
+import AboutMe from './Components/AboutMe/AboutMe';
+import MyTech from './Components/MyTech/MyTech';
+import MyProjects from './Components/MyProjects/MyProjects';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <header className="App-header">
+        <Nav />
         <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/tech" element={<MyTech />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/contact" element={<Contact />} />
+        
 {/*         <Route path="/*" element={<Header />} /> */}
         </Routes>
       </header>
