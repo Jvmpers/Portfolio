4abaf5e6db54857fd97f87b2dd686d1282259683
Jvmpers <tomaspastore200111@gmail.com>
2022-11-16 21:10:41 -0300

    a href

--- my-portfolio/src/Components/AboutMe/AboutMe.tsx
+++ my-portfolio/src/Components/AboutMe/AboutMe.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import style from "./AboutMe.module.css";
 export default function AboutMe() {
   return (
-    <section className={style.div}>
+    <section id="aboutMe" className={style.div}>
           <div className={style.textAbout}>
-            <h1 className={style.about}>About Me</h1>
+            <h1  className={style.about}>About Me</h1>
             <p className={style.descriptionMe}>
               Full Stack Web Developer. I am looking for a job where I can train and learn new technologies and be part of a work team. I characterize myself as an empathetic, cooperative, tenacious person and solver of logical and workgroup problems.
             </p>
--- my-portfolio/src/Components/MyTech/MyTech.tsx
+++ my-portfolio/src/Components/MyTech/MyTech.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import style from "./MyTech.module.css";
 export default function MyTech() {
   return (
+    <section id="myTech" className={style.sectionMyTech}>
     <div>MyTech</div>
+    </section>
+
   )
 }
